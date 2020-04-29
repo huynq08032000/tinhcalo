af1cef30a1242a1c9ff5c1db447e28ac31d333dc
huynq08032000 <55910030+huynq08032000@users.noreply.github.com>
2020-04-29 11:40:01 +0700

    commit

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 window.onload = init 
 function init(){
     let form=document.getElementById('form-tinh')
-    form.onsubmit=formlogInSubmitHandler
-    function formlogInSubmitHandler(){
+    form.onsubmit=formTinhSubmitHandler
+    form.onreset = ()=>{
+        setText('currentName','')
+        setText('RMR','')
+        setText('AMR','')
+        setText('resultEX','')
+        setText('TMR','')
+        setText('CLKD','')
+
+    }
+    function formTinhSubmitHandler(){
         event.preventDefault()
         //1.Get infor
         let tinhInfor={
@@ -11,30 +20,54 @@ function init(){
            age : form.age.value,
            weight : form.weight.value,
            height : form.height.value,
-           ex : form.ex.value
+           ex : form.ex.value,
+           dream : form.dream.value
         }
         //2. validate Infor
         let validateResult=[
             validate(tinhInfor.currentName,'name-error',"Vui lòng nhập tên!"),
-            validate(tinhInfor.sex.toUpperCase()==='NAM'|| tinhInfor.sex.toUpperCase()==='NỮ','sex-error',"Vui lòng nhập đúng giới tính nam hoặc nữ!"),
+            validate(tinhInfor.sex,'sex-error',"Vui lòng nhập đúng giới tính nam hoặc nữ!"),
             validate(tinhInfor.age && tinhInfor.age>0 ,'age-error','Vui lòng nhập tuổi'),
             validate(tinhInfor.weight && tinhInfor.weight>0,'weight-error','Vui lòng nhập cân nặng'),
             validate(tinhInfor.height && tinhInfor.height>10,'height-error','Vui lòng nhập đúng chiều cao!'),
-            validate(tinhInfor.ex && tinhInfor.ex>=0,'ex-error',"Vui lòng nhập năng lượng tập thể dục!")
+            validate(tinhInfor.ex && tinhInfor.ex>=0,'ex-error',"Vui lòng nhập năng lượng tập thể dục!"),
+            validate(tinhInfor.dream,'dream-error','Vui lòng nhập mong muốn của bản thân!')
         ]           
         if(allPassed(validateResult)){
-            let RMR = 9.99*tinhInfor.weight + 6.25*tinhInfor.height - 4.92*tinhInfor.age+5
-            let AMR = 0;
-            if(tinhInfor.sex.toUpperCase()==='NAM'){
-                AMR = RMR/100*30;
+            let chiSo ={
+                RMR : 9.99*tinhInfor.weight + 6.25*tinhInfor.height - 4.92*tinhInfor.age+5,
+                AMR : 0,
+                EX : tinhInfor.ex,
+                TMR : 0,
+                CLKD : 0
+            }
+            if(tinhInfor.sex==='nam'){
+                chiSo.AMR = chiSo.RMR/100*30;
             } else{
-                AMR = RMR/100*25;
+                chiSo.AMR = chiSo.RMR/100*25;
+            }
+            chiSo.TMR=Number(chiSo.RMR + chiSo.AMR + chiSo.EX)
+            if (tinhInfor.dream === 'Giữ cân'){
+                chiSo.CLKD = chiSo.TMR
+            } else if(tinhInfor.dream === 'Giảm cân'){
+                if(tinhInfor.age<40){
+                    chiSo.CLKD=chiSo.TMR-500;
+                } else{
+                    chiSo.CLKD=chiSo.TMR-300
+                }
+            } else {
+                if(tinhInfor.age<40){
+                    chiSo.CLKD=chiSo.TMR+500;
+                } else{
+                    chiSo.CLKD=chiSo.TMR+300
+                }
             }
             setText('currentName',tinhInfor.currentName.toUpperCase())
-            setText('RMR',RMR)
-            setText('AMR',AMR)
-            setText('resultEX',tinhInfor.ex)
-            setText('TMR',RMR+AMR+tinhInfor.ex)
+            setText('RMR',chiSo.RMR)
+            setText('AMR',chiSo.AMR)
+            setText('resultEX',chiSo.EX)
+            setText('TMR',chiSo.TMR)
+            setText('CLKD',chiSo.CLKD)
         }
     }    
     setText = function(id,text){
